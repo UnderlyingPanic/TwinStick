04ee6b0bf351cace8c5e839f6f761d1366475e0e
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-03 20:03:49 +0100

    Added Replay Playback and Record

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	public bool recording = true;
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (CrossPlatformInputManager.GetButton("Fire1")) {
+			recording = false;
+		} else {
+			recording = true;
+		}
 	}
 }
--- Assets/ReplaySystem.cs
+++ Assets/ReplaySystem.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class ReplaySystem : MonoBehaviour {
 
-	private const int bufferFrames = 100;
+	private const int bufferFrames = 1000;
 	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
 
+	private GameManager gameManager;
 	private Rigidbody rigidBody;
+	private bool recording;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody> ();
+		gameManager = GameObject.FindObjectOfType<GameManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Record ();
+		if (gameManager.recording) {
+			Record ();
+		} else {
+			PlayBack ();
+		}
 	}
 
 	void Record () {
