87a2c08f3abe029a47f8f24e63067f133133aaad
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-03 19:21:17 +0100

    Pre GameManager

---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Record ();
+	}
+
+	void Record () {
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+		rigidBody.isKinematic = false;
+	}
+
+	void PlayBack () {
+		rigidBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		print ("Reading Frame " + frame);
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+}
+
+
+
+
+/// <summary>
+/// A Structure for storing time, rotation, and position
+/// </summary>
+public struct MyKeyFrame {
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation) {
+		frameTime = aTime;
+		position = aPosition;
+		rotation = aRotation;
+	}
+}
