50ef426793a60dd45c98490efae98e239540f9d1
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-06-04 14:27:08 +0100

    Added second stick for camera (or arrows for Keyboard)

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+
+	void LateUpdate () {
+		transform.LookAt (player.transform);
+	}
+}
